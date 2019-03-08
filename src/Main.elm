ba8b26ac6794454316ce80b35843e02ba8c30cf6
onevcat <onevcat@gmail.com>
2019-03-08 12:42:15 +0900

    Define model

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,18 @@
+import Array exposing (Array)
 import Html exposing (..)
 
 main =
     text "text"
+
+type alias Model =
+    { board : Board
+    }
+
+type alias Board = Array Row
+type alias Row = Array Cell
+type Cell
+    = Tile Int
+    | Empty
+
+
+
